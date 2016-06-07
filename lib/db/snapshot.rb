fea53f5c6a62e44e2f6a64ef9729f98b60226ec8
Ed Mckain <mckain.default@gmail.com>
2016-06-06 18:30:03 -0600

    adds basic & general methods to Snapshot

--- lib/db/snapshot.rb
+++ lib/db/snapshot.rb
@@ -2,6 +2,27 @@ require "db/snapshot/version"
 
 module Db
   module Snapshot
-    # Your code goes here...
+    def list
+      system('ls -la -tr')
+    end
+
+    def create(snapshot_name = '')
+      puts "Hey World!: #{dump}"
+    end
+
+    def travel_to(specific_date_time = '')
+      restore(specific_date_time)
+    end
+
+    private
+
+    def dump
+      date_time = Time.now.strftime("%Y%m%d_%H%M")
+      system("mongodump -o dump/#{date_time}")
+    end
+
+    def restore(specific_date_time = '')
+      system("mongorestore #{specific_date_time}")
+    end
   end
 end
