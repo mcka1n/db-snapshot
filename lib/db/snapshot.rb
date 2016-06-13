ad91a828f1ac91ad790e3938fd448a964bfb3351
Ed Mckain <mckain.default@gmail.com>
2016-06-12 21:25:28 -0600

    uses dump/ folder by default

--- lib/db/snapshot.rb
+++ lib/db/snapshot.rb
@@ -2,27 +2,25 @@ require "db/snapshot/version"
 
 module Db
   module Snapshot
-    def list
-      system('ls -la -tr')
+    def self.list
+      system('ls -la -tr dump/')
     end
 
-    def create(snapshot_name = '')
-      dump
+    def self.create(snapshot_name = '')
+      date_time = Time.now.strftime("%Y%m%d_%H%M")
+      database_name = Mongoid.default_session.options[:database]
+      system("mongodump --db #{database_name} -o dump/#{date_time}")
     end
 
-    def travel_to(specific_date_time = '')
-      restore(specific_date_time)
+    def self.travel_to(specific_date_time = '')
+      self.restore(specific_date_time)
     end
 
     private
 
-    def dump
-      date_time = Time.now.strftime("%Y%m%d_%H%M")
-      system("mongodump -o dump/#{date_time}")
-    end
-
-    def restore(specific_date_time = '')
-      system("mongorestore #{specific_date_time}")
+    def self.restore(specific_date_time = '')
+      database_name = Mongoid.default_session.options[:database]
+      system("mongorestore --db #{database_name} dump/#{specific_date_time}/#{database_name}")
     end
   end
 end
