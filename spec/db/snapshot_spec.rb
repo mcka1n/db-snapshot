3bcef383e45221d0df4611536b39b05ddfe2d154
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2018-10-22 21:57:30 -0600

    Add specs for class methods

--- spec/db/snapshot_spec.rb
+++ spec/db/snapshot_spec.rb
@@ -5,7 +5,34 @@ describe Db::Snapshot do
     expect(Db::Snapshot::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    # pending
+  describe '.list' do
+    it 'returns the correct system command' do
+      expect_any_instance_of(Kernel)
+        .to receive(:system)
+        .with('ls -la -tr dump/')
+
+        Db::Snapshot.list
+    end
+  end
+
+  describe '.create' do
+    context 'when snapshot_name is not an empty string' do
+      it 'returns the correct system command' do
+        expect_any_instance_of(Kernel)
+          .to receive(:system)
+
+        Db::Snapshot.create("HELLO")
+      end
+    end
+  end
+
+  describe '.travel_to' do
+    it 'calls the restore method' do
+      expect(described_class)
+        .to receive(:restore)
+        .with('20160612_2115')
+
+      Db::Snapshot.travel_to('20160612_2115')
+    end
   end
 end
